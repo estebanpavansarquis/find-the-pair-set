ea51df97fdd5fa99513ec55eff297069c5eb8bc1
EstebanPS <estebanpavansarquis@gmail.com>
2021-03-30 22:20:19 -0300

    main package added
    Signed-off-by: EstebanPS <estebanpavansarquis@gmail.com>

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main () {
+
+	// FindThePairSet function call
+	findThePairSetInpunt := "asd"
+	fmt.Println("My input for FindThePairSet is: ", findThePairSetInpunt)
+	fmt.Println("Result of running FindThePairSet: ", findThePairSetInpunt)
+
+}
